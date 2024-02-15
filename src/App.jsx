b47807d3582cf302108e80145c850b9af5740681
quinnasher <pauldivineagionoja@gmail.com>
2024-02-15 16:56:42 +0100

    fixed custom color

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
+import HowTO from "./Components/HowTO.jsx";
+import React from "react";
 
 function App() {
-
   return (
     <>
+      <HowTO></HowTO>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
