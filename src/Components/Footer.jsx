8fcc8b62f72a4d2117aa5b3292055ffa5d55ce1d
Elrey009 <daniloejike@gmail.com>
2024-02-18 16:07:38 +0100

    Modified src

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,13 +4,13 @@ import logo from "../assets/logo.png";
 
 function Footer() {
   return (
-    <footer className=" flex  h-300 w-full items-center justify-center  gap-2 rounded-tl-2xl  rounded-tr-2xl bg-celestialBlue  py-8 font-sans text-whiteSmoke opacity-[0.6] md:justify-evenly    md:opacity-[1] ">
-      <div className=" mx-1 h-5 w-12 md:h-32 md:w-40 ">
-        <img src={logo} className=" h-18 w-12" />
+    <footer className=" relative  flex h-full w-full items-center justify-between gap-2 rounded-tl-3xl  rounded-tr-3xl bg-celestialBlue  bg-opacity-[0.6]  py-8 text-whiteSmoke  md:justify-around md:bg-opacity-[1] md:p-6">
+      <div className=" mx-4 md:mb-8 lg:mr-24">
+        <img src={logo} />
       </div>
-      <div className=" ml-4 whitespace-nowrap  md:mr-[18rem]">
-        <h6 className=" font-noraml  mb-2 text-xl ">Quick links</h6>
-        <ul className="  list-none text-[14px]  leading-8">
+      <div className=" mr-4  whitespace-nowrap md:mr-[5rem] md:mt-8 md:text-xl">
+        <h6 className=" mb-2  text-sm font-semibold md:text-xl">Quick links</h6>
+        <ul className="  list-none text-[12px] font-normal leading-6 md:text-[16px]">
           <a href="">
             {" "}
             <li className="">Home</li>
@@ -23,24 +23,24 @@ function Footer() {
           </a>
         </ul>
       </div>
-      <div className=" ml-7 flex flex-col gap-3  whitespace-nowrap  text-[14px] md:mr-4 md:flex-row md:justify-evenly md:gap-[2em] ">
-        <div className=" leading-8 md:mb-8 md:mr-24 ">
-          <h6 className=" mb-3 text-2xl font-normal">Support</h6>
+      <div className=" mr-2 flex flex-col gap-1 text-[12px]  md:mt-8  md:flex-row md:justify-evenly md:gap-[3rem]  md:text-[16px]">
+        <div className=" whitespace-nowrap leading-6 md:mb-8 md:mr-[3rem]">
+          <h6 className=" mb-3 text-sm font-semibold md:text-xl">Support</h6>
           <ul className=" list-none">
             <li>08001020300</li>
             <li>support@blockfake.com</li>
           </ul>
         </div>
         {/* icons */}
-        <div className=" mt-2 flex w-24 gap-2 ">
-          <div className=" cursor-pointer rounded-full bg-slate-50  p-2.5  text-xl text-[#00436F] md:h-10">
+        <div className=" mt-2 flex  gap-2  md:mt-0 lg:ml-24">
+          <div className=" h-8 w-8 cursor-pointer rounded-full bg-slate-50  p-2  text-[18px] text-[#00436F] md:h-10 md:w-10 md:text-2xl">
             <FaTwitter />
           </div>
 
-          <div className=" cursor-pointer rounded-full bg-slate-50  p-2.5  text-xl text-[#00436F] md:h-10">
+          <div className=" h-8 w-8 cursor-pointer rounded-full bg-slate-50 p-2  text-[18px] text-[#00436F] md:h-10 md:w-10 md:text-2xl">
             <FaFacebook />
           </div>
-          <div className=" cursor-pointer rounded-full bg-slate-50  p-2.5  text-xl text-[#00436F] md:h-10">
+          <div className=" h-8 w-8 cursor-pointer rounded-full bg-slate-50 p-2  text-[18px] text-[#00436F] md:h-10 md:w-10 md:text-2xl">
             <FaInstagram />
           </div>
         </div>
